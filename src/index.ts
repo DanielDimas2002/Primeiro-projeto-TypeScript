615c4f2d9ad8d65eda31beca5cd2ffd8a1ea964c
Daniel Dimas de Souza <101479145+DanielDimas2002@users.noreply.github.com>
2024-08-29 09:52:31 -0300

    Conclusão

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import Person from "./Classes/Person";
+
+const p = new Person("Maria", 25)
+
+console.log(p)
+
+alert("Alerta vindo do TypeScript")
+
+console.log(p.age)
+console.log(p.name)
+
+alert(`Nome: ${p.name}, Idade: ${p.age}`);
